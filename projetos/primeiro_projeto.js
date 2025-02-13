223243447c94404728e9c63f628a6fb36831e21d
Filipe Albuquerque <filipi_98@hotmail.com>
2025-02-13 13:46:13 -0300

    meu segundo commit

--- projetos/primeiro_projeto.js
+++ projetos/primeiro_projeto.js
@@ -1,1 +1,41 @@
-console.log("Ola mundão")
+let nomeHeroi = "Celebrinor"
+let xpHeroi = 5.000
+
+switch(true){
+    case xpHeroi < 1.000:
+        console.log("Ferro")
+    break
+
+    case xpHeroi >= 1.001 && xpHeroi <= 2.000:
+        console.log("Bronze")
+    break
+
+    case xpHeroi >= 2.001 && xpHeroi <= 5.000:
+        console.log("Prata")
+    break
+
+    case xpHeroi >= 5.001 && xpHeroi <= 7.000:
+        console.log("Ouro")
+    break
+
+    case xpHeroi >= 7.001 && xpHeroi <= 8.000:
+        console.log("Platina")
+    break
+
+    case xpHeroi >= 8.001 && xpHeroi <= 9.000:
+        console.log("Ascendente")
+    break
+
+    case xpHeroi >= 9.001 && xpHeroi <= 10.000:
+        console.log(xpHeroi + " Imortal")
+    break
+
+    case xpHeroi >= 10.001:
+        console.log("Radiante")
+    break
+
+    default:
+        console.log("Sistema não conseguiu calcular o XP do Heroi " + nomeHeroi)
+}
+
+console.log("O herói de nome " + nomeHeroi + " está no nivel de " + xpHeroi)
